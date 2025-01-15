2baed1b4181cbad0bc8be4cb8ca61cf5414d5847
Ashwin <ashwinmehta1234500@gmail.com>
2025-01-16 00:20:51 +0530

    setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "End-to-End Text-Summarizer-Project"
+REPO_NAME = "End-to-End-Text-Summarizater-Project"
 AUTHOR_USER_NAME = "Ashwin Mehta"
 SRC_REPO = "textSummarizer"
 AUTHOR_EMAIL = "ashwinmehta1234500@gmail.com"
